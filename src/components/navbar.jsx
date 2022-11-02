62fa9b4d0c41e95665bc7b481ddfd548cc08d3e9
ypradafah28 <47551707+ypradafah28@users.noreply.github.com>
2022-11-02 14:45:13 +0700

    update

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -6,8 +6,8 @@ function Navbar() {
             <figure className='absolute z-10 top-0 left-0 w-full' >
                 <img className='w-full object-cover' src="./bunaken.png" alt="" />
             </figure>
-            <section className='h-[6.5rem] py-8 z-20 absolute right-16'>
-                <ul className='flex gap-6 font-sans text-base text-right  mr-60'>
+            <section className='h-[6.5rem] py-8 z-20 absolute right-16 flex'>
+                <ul id='list' className='flex gap-6 font-sans text-base text-right mr-60'>
                     <li><a href="/#"> Home </a></li>
                     <li><a href="/#"> Services </a></li>
                     <li><a href="/#"> About </a></li>
